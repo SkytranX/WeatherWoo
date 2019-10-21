fc4ddc36b4690c98107d6f21a52934d4c449dc4a
Steven Tran <StevenSkytran@gmail.com>
2019-10-21 14:09:24 -0400

    Initial mvvm Commit

--- app/src/main/java/com/example/weatherwoo/view/MainActivity.java
+++ app/src/main/java/com/example/weatherwoo/view/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.weatherwoo;
+package com.example.weatherwoo.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.weatherwoo.R;
 
 public class MainActivity extends AppCompatActivity {
 
